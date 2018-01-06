4b9b34066594a83da80581ca95f4aee57e61a275
Terrence Brannon <metaperl@gmail.com>
2018-01-06 06:30:28 -0500

    Improve docs

--- src/tasks.py
+++ src/tasks.py
@@ -1,10 +1,20 @@
 """Define a collection of tasks invokable from the command-line/cron.
 
 This module leverages `PyInvoke`_ to create tasks relevant ot the execution
-of SurgeTrader.
+of SurgeTrader. The tasks are ordered in this module in the order that you
+would typically use them:
+    1. you would download the market data via `invoke download`
+    1. The next hour you would download the market data again.
+    1. Then `invoke buy` to buy the coins with the strongest gain.
+    1. Then `invoke takeprofit` to set profit targets for your buys.
+    1. Then `invoke profitreport -d yesterday` to see what profits you made
+    1. Then `invoke cancelsells` to cancel all sell orders so that
+    `invoke takeprofit` then re-issue them.
+    1. On rare occassions, you might liquidate all coins in your portfolio,
+    cancelling all open sell orders. This is usually done when you are
+    restarting / abandoning SurgeTrader usage.
 
 Example:
-
 
         $ invoke download
 
@@ -40,60 +50,6 @@ def listify_ini(ini):
         inis = users.inis()
 
     return inis
-
-
-@task
-def profitreport(_ctx, ini=None, date_string=None, skip_markets=None):
-    """Generate and email a profit report for a certain time frame.
-
-    Args:
-        ini (str): The name of an ini file or a falsy value.
-        date_string : "yesterday" and "lastmonth" are valid values
-        skip_markets: Coins to exclude from calculating the profit report.
-            This is used when a market is under maintenance becauase at that
-            point the exchange API does not return data for that coin.
-
-    Returns:
-        Nothing. It dumps a csv and html of the email profit report in src/tmp.
-
-    """
-    import lib.report.profit
-
-
-    print("tasks.SKIP MARKETS={}".format(skip_markets))
-
-
-    inis = listify_ini(ini)
-
-    if date_string:
-        from datetime import date
-        if date_string == 'yesterday':
-            date_string = "Yesterday"
-            _date = date.fromordinal(date.today().toordinal()-1)
-        elif date_string == 'lastmonth':
-            date_string = "Last month"
-            from dateutil.relativedelta import relativedelta
-            today = date.today()
-            diff = today - relativedelta(months=1)
-            start_of_last_month = date(diff.year, diff.month, 1)
-            end_of_last_month = date(today.year, today.month, 1) - relativedelta(days=1)
-            print("Date range for profit report. Start={}. End={}".format(
-                start_of_last_month, end_of_last_month))
-            _date = [start_of_last_month, end_of_last_month]
-        else:
-            raise Exception("Unrecognized date option")
-    else:
-        _date = None
-
-    if skip_markets:
-        skip_markets = skip_markets.split()
-
-    print("tasks2.SKIP MARKETS={}".format(skip_markets))
-
-    for user_ini in inis:
-        print("Processing {}".format(user_ini))
-        lib.report.profit.main(user_ini, date_string, _date=_date, skip_markets=skip_markets)
-
 
 @task
 def download(_ctx):
@@ -150,6 +106,59 @@ def takeprofit(_ctx, ini=None):
     for _ in inis:
         print("Processing {}".format(_))
         _takeprofit.take_profit(_)
+
+@task
+def profitreport(_ctx, ini=None, date_string=None, skip_markets=None):
+    """Generate and email a profit report for a certain time frame.
+
+    Args:
+        ini (str): The name of an ini file or a falsy value.
+        date_string : "yesterday" and "lastmonth" are valid values
+        skip_markets: Coins to exclude from calculating the profit report.
+            This is used when a market is under maintenance becauase at that
+            point the exchange API does not return data for that coin.
+
+    Returns:
+        Nothing. It dumps a csv and html of the email profit report in src/tmp.
+
+    """
+    import lib.report.profit
+
+
+    print("tasks.SKIP MARKETS={}".format(skip_markets))
+
+
+    inis = listify_ini(ini)
+
+    if date_string:
+        from datetime import date
+        if date_string == 'yesterday':
+            date_string = "Yesterday"
+            _date = date.fromordinal(date.today().toordinal()-1)
+        elif date_string == 'lastmonth':
+            date_string = "Last month"
+            from dateutil.relativedelta import relativedelta
+            today = date.today()
+            diff = today - relativedelta(months=1)
+            start_of_last_month = date(diff.year, diff.month, 1)
+            end_of_last_month = date(today.year, today.month, 1) - relativedelta(days=1)
+            print("Date range for profit report. Start={}. End={}".format(
+                start_of_last_month, end_of_last_month))
+            _date = [start_of_last_month, end_of_last_month]
+        else:
+            raise Exception("Unrecognized date option")
+    else:
+        _date = None
+
+    if skip_markets:
+        skip_markets = skip_markets.split()
+
+    print("tasks2.SKIP MARKETS={}".format(skip_markets))
+
+    for user_ini in inis:
+        print("Processing {}".format(user_ini))
+        lib.report.profit.main(user_ini, date_string, _date=_date, skip_markets=skip_markets)
+
 
 
 @task
